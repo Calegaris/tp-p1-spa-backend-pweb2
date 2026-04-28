a4133a0c3abd42d2dfb748df58876d46a64d0d9a
Luis Calegari <lcalegari97@gmail.com>
2026-04-27 22:20:05 -0300

    feat(services): Initialize services

---
+++ services/cart_service.py
@@ -0,0 +1,10 @@
+# Lógica de negocio del carrito
+
+def add_item(product_id, quantity):
+    pass
+
+def remove_item(product_id):
+    pass
+
+def calculate_total():
+    pass
---
+++ services/products_service.py
@@ -0,0 +1,4 @@
+# Lógica de negocio de productos
+
+def load_products():
+    pass
