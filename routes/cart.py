7fdcd562e83e75ee501b8d7d06a03fb35f71b29f
Luis Calegari <lcalegari97@gmail.com>
2026-04-27 22:13:28 -0300

    feat(routes): initialize blueprints

---
+++ routes/cart.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+cart_bp = Blueprint("cart", __name__)
+
+# Las rutas se implementarán en la feature correspondiente
---
+++ routes/products.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+products_bp = Blueprint("products", __name__)
+
+# Las rutas se implementarán en la feature correspondiente
