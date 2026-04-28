4814e44839e55892e5adca574129f730543f7bd9
Luis Calegari <lcalegari97@gmail.com>
2026-04-27 22:16:07 -0300

    feat(controllers): Initialize controllers

---
+++ controllers/cart_controller.py
@@ -0,0 +1,10 @@
+# Controlador del carrito
+
+def add_to_cart(product_id, quantity):
+    pass
+
+def remove_from_cart(product_id):
+    pass
+
+def get_cart_total():
+    pass
---
+++ controllers/products_controller.py
@@ -0,0 +1,5 @@
+# Controlador de productos
+# Acá se coordina la request con el servicio
+
+def get_all_products():
+    pass
